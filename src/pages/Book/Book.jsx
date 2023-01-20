6efa3881df55c872d2f4ae59c18a33e7bb6b35f1
arinzehills <arinzehill@gmail.com>
2023-01-20 20:07:16 +0100

    fixes

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,11 +1,56 @@
 import React, { useState } from "react";
+import { AiFillDelete } from "react-icons/ai";
 import Button from "../../components/Button/Button";
 import InputField from "../../components/Inputfield/InputField";
 import handleChange from "../../utils/handleChange";
 
 const Book = () => {
-  const initialValues = { email: "", password: "" };
+  const initialValues = { name: "", age: "" };
   const [formValues, setFormValues] = useState(initialValues);
+  const [travellers, setTravellers] = useState([]);
+  const [error, setError] = useState([]);
+  const addTravellers = () => {
+    console.log({ ...formValues });
+    setTravellers([...travellers, formValues]);
+    // setFormValues([{ ...initialValues }]);
+  };
+  console.log(travellers);
+  const ListTravelers = () => {
+    return (
+      <div
+        className="destination_list"
+        style={{
+          width: "50%",
+          overflow: "visible",
+        }}
+      >
+        {travellers.map((traveller, index) => (
+          <div
+            style={{
+              height: "50px",
+              maxWidth: "150px",
+              background: "var(--orange-gradient)",
+              color: "white",
+              margin: "1rem",
+              padding: "1rem",
+            }}
+            className=""
+          >
+            <div
+              //   className="centerClass"
+              style={{
+                background: "white",
+                borderRadius: "5px",
+              }}
+            >
+              <AiFillDelete color="red" />
+            </div>
+            <div>{traveller.name}</div>
+          </div>
+        ))}
+      </div>
+    );
+  };
   return (
     <div
       className="reg-fsc-section "
@@ -17,6 +62,7 @@ const Book = () => {
           height: "80vh",
           boxShadow: "var(--box-shadow)",
           background: "white",
+          display: "flex",
         }}
       >
         {/* formfield container */}
@@ -25,45 +71,64 @@ const Book = () => {
             <h3 style={{ textTransform: "capitalize" }}>Name</h3>
             <div style={{ flexDirection: "column" }}>
               <InputField
-                label={"Enter email"}
-                value={formValues.email}
-                name={"email"}
+                label={"Enter name"}
+                value={formValues.name}
+                name={"name"}
                 inputStyle="input--shadow-purple"
                 inputColor="purple-input"
                 onHandleChange={(e) => {
                   handleChange(e, formValues, setFormValues);
                 }}
               />
-              {/* <p className="errors">{formErrors.email}</p> */}
+              {/* <p className="errors">{formErrors.name}</p> */}
             </div>
           </div>
           <div className="register_row">
             <h3 style={{ textTransform: "capitalize" }}>Age</h3>
             <div style={{ flexDirection: "column" }}>
               <InputField
-                label={"Enter email"}
-                value={formValues.email}
-                name={"email"}
+                label={"Enter age"}
+                value={formValues.age}
+                name={"age"}
                 inputStyle="input--shadow-purple"
                 inputColor="purple-input"
                 onHandleChange={(e) => {
                   handleChange(e, formValues, setFormValues);
                 }}
               />
-              {/* <p className="errors">{formErrors.email}</p> */}
+              <p className="errors">{error}</p>
             </div>
             <div className="centerClass">
               <Button
                 buttonColor={"btn--orange"}
-                // onClick={onSubmit}
+                onClick={() => {
+                  !formValues.name || !formValues.password
+                    ? setError("Fields are required")
+                    : setError("");
+                  formValues.name || formValues.password
+                    ? addTravellers()
+                    : null;
+                }}
               >
                 Add
               </Button>
+              <Button
+                buttonColor={"btn--orange"}
+                // onClick={onSubmit}
+              >
+                {"Proceed->"}
+              </Button>
             </div>
+            {travellers.length > 0 && <ListTravelers />}
           </div>
 
           {/* end of form container */}
-          <div style={{ background: "red", width: "50%" }}>dss</div>
+        </div>
+        <div
+          style={{ background: "var(--success)", width: "50%" }}
+          className="centerClass"
+        >
+          dss
         </div>
       </div>
     </div>
--- src/utils/handleChange.js
+++ src/utils/handleChange.js
@@ -1,6 +1,4 @@
 const handleChange = (e, formValues, setFormValues) => {
-  console.log(e);
-  console.log(e.target);
   const { name, value } = e.target;
   setFormValues({ ...formValues, [name]: value });
 
